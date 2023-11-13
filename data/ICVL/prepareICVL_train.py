776978a86161dcb2bc97c5697f254efa24392c65
mehalcoding <150694606+mehalcoding@users.noreply.github.com>
2023-11-13 20:01:13 +0530

    Update prepareICVL_train.py

--- data/ICVL/prepareICVL_train.py
+++ data/ICVL/prepareICVL_train.py
@@ -479,7 +479,7 @@ class ICVLImporter(DepthImporter):
 
         # Load the dataset
         objdir = '{}/Depth/'.format(self.basepath)
-        trainlabels = '{}/labels.txt'.format(self.basepath)
+        trainlabels = "/kaggle/input/icvl-dataset-full/Training/labels.txt"
         
         f=open("icvl_train_list.txt", "r")
         ll=f.readlines()
